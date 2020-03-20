fe714b0a1db3b65134504053361ac808019cb3df
Webbuildermn <gerald.ryan40@yahoo.com>
2020-03-20 12:14:36 -0500

    maybe close to or at mvp but lets see what we can add. dang i hate cutting it close

--- starwars/src/components/Post.js
+++ starwars/src/components/Post.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { Card, Button, CardImg, CardSubtitle, CardHeader, CardFooter, CardBody, CardTitle, CardText } from 'reactstrap'
+import './Post.css'
 
 export default function Post(e)
 {
 
   return (
     <div>
-
-      {`Name: ${e.char.name} `}
-      {`Height: ${e.char.height} `}
-      {`Mass: ${e.char.mass} `}
+      <Card>
+        {/* <CardImg top width="100%" src={e.char.url} alt="Card image cap" /> */}
+        <CardBody>
+          <CardTitle>{e.char.name}</CardTitle>
+          <CardSubtitle>{e.char.gender}</CardSubtitle>
+          <CardText>        {`Height: ${e.char.height} `}
+        {`Mass: ${e.char.mass} `}</CardText>
+        </CardBody>
+      </Card>
+        {/* {`Name: ${e.char.name} `}
+        {`Height: ${e.char.height} `}
+        {`Mass: ${e.char.mass} `} */}
+ 
       {/* {`Name: ${e.char.name} `}
       {`Name: ${e.char.name} `}
       {`Name: ${e.char.name} `}
@@ -18,7 +29,7 @@ export default function Post(e)
 
 
 
-  }
+      }
     </div>
   )
 
