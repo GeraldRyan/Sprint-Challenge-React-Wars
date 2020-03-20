2218d8693180e0d8a9e26c2f9cbe6a821424a543
Webbuildermn <gerald.ryan40@yahoo.com>
2020-03-20 10:52:35 -0500

    why doesnt

--- starwars/src/components/Crew.js
+++ starwars/src/components/Crew.js
@@ -4,9 +4,9 @@ import axios from 'axios'
 
 export default function Crew(props)
 {
-  console.log(props.data)
+  console.log(props.data.results)
   return (
-    < div > hello world</div >
+    < div > {props.data}</div >
   )
 }
 
